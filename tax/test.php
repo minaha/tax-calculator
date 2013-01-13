66bc1b31f9caf31f0217392fdab3ea817dc18cdc
y.umezaki <bungoume3@yahoo.co.jp>
2013-01-13 17:27:08 +0900

    make cache directory

--- tax/test.php
+++ tax/test.php
@@ -5,10 +5,9 @@ Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('/templates');
 $twig = new Twig_Environment($loader, array(
-    'cache' => '/path/to/compilation_cache',
+    'cache' => '/compilation_cache',
 ));
 
 echo $twig->render('index.html', array('name' => 'Fabien'));
-
 
 ?>
